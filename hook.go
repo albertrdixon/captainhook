93bb41d2b939e021437250140222d38381baf9e8
albertrdixon <albert@timelinelabs.com>
2015-03-19 15:27:27 -0700

    Use ioutil.ReadAll to guarantee the entire request.Body is read.

--- hook.go
+++ hook.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
-	"log"
-	"net"
-	"net/http"
-	"strings"
+  "encoding/json"
+  "io"
+  "io/ioutil"
+  "log"
+  "net"
+  "net/http"
+  "strings"
 
-	"github.com/gorilla/mux"
+  "github.com/gorilla/mux"
 )
 
 func hookHandler(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id := params["id"]
-	log.Printf("Received hook for id '%s' from %s\n", id, r.RemoteAddr)
-	rb, err := NewRunBook(id)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	remoteIP := net.ParseIP(strings.Split(r.RemoteAddr, ":")[0])
-	if !rb.AddrIsAllowed(remoteIP) {
-		log.Printf("Hook id '%s' is not allowed from %v\n", id, r.RemoteAddr)
-		http.Error(w, "Not authorized.", http.StatusUnauthorized)
-		return
-	}
-	interoplatePOSTData(rb, r)
-	response, err := rb.execute()
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	if echo {
-		data, err := json.MarshalIndent(response, "", "  ")
-		if err != nil {
-			log.Println(err.Error())
-		}
-		w.Write(data)
-	}
+  params := mux.Vars(r)
+  id := params["id"]
+  log.Printf("Received hook for id '%s' from %s\n", id, r.RemoteAddr)
+  rb, err := NewRunBook(id)
+  if err != nil {
+    log.Println(err.Error())
+    http.Error(w, err.Error(), 500)
+    return
+  }
+  remoteIP := net.ParseIP(strings.Split(r.RemoteAddr, ":")[0])
+  if !rb.AddrIsAllowed(remoteIP) {
+    log.Printf("Hook id '%s' is not allowed from %v\n", id, r.RemoteAddr)
+    http.Error(w, "Not authorized.", http.StatusUnauthorized)
+    return
+  }
+  interoplatePOSTData(rb, r)
+  response, err := rb.execute()
+  if err != nil {
+    log.Println(err.Error())
+    http.Error(w, err.Error(), 500)
+    return
+  }
+  if echo {
+    data, err := json.MarshalIndent(response, "", "  ")
+    if err != nil {
+      log.Println(err.Error())
+    }
+    w.Write(data)
+  }
 }
 
 func interoplatePOSTData(rb *runBook, r *http.Request) {
-	if r.ContentLength == 0 {
-		return
-	}
-	data := make([]byte, r.ContentLength)
-	_, err := r.Body.Read(data)
-	if err != nil && err != io.EOF {
-		log.Fatal(err)
-		return
-	}
-	defer r.Body.Close()
-	stringData := string(data[:r.ContentLength])
-	for i := range rb.Scripts {
-		for j := range rb.Scripts[i].Args {
-			rb.Scripts[i].Args[j] = strings.Replace(rb.Scripts[i].Args[j], "{{POST}}", stringData, -1)
-		}
-	}
+  if r.ContentLength == 0 {
+    return
+  }
+  data, err := ioutil.ReadAll(r.Body)
+  if err != nil && err != io.EOF {
+    log.Fatal(err)
+    return
+  }
+  defer r.Body.Close()
+  stringData := string(data[:r.ContentLength])
+  for i := range rb.Scripts {
+    for j := range rb.Scripts[i].Args {
+      rb.Scripts[i].Args[j] = strings.Replace(rb.Scripts[i].Args[j], "{{POST}}", stringData, -1)
+    }
+  }
 }
